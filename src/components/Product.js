6657abac263b270515850bce2feda45fd2cfb5ba
jose6rojas <jose99rojas@hotmail.com>
2019-02-17 15:52:28 -0600

    implementando el componente ProductList

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 export default class Product extends Component {
-    render() {
-        return (
-            <div>
-                <h3>hello from Product</h3>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h3>hello from Product</h3>
+      </div>
+    );
+  }
 }
--- src/data.js
+++ src/data.js
@@ -58,15 +58,27 @@ export const storeProducts = [
     inCart: false,
     count: 0,
     total: 0
+  },
+  {
+    id: 6,
+    title: "Palomitas Gourmet",
+    img: "img/producto6.jpg",
+    price: 50,
+    company: "Palomitas Gourmet",
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
   }
 ];
 
 export const detailProduct = {
   id: 1,
-  title: "Google Pixel - Black",
-  img: "img/product-1.png",
-  price: 10,
-  company: "google",
+  title: "Mancile Café",
+  img: "img/producto1.jpg",
+  price: 100,
+  company: "Mancile Café",
   info:
     "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
   inCart: false,
