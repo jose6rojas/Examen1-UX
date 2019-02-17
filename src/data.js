02c93bc82432bf30e073e04491a43406e9568b13
jose6rojas <jose99rojas@hotmail.com>
2019-02-17 16:44:53 -0600

    cambiando las imágenes de los productos y agregando algunas imágenes alternativas

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const storeProducts = [
   {
     id: 1,
     title: "Mancile Café",
-    img: "img/producto1.jpg",
+    img: "img/producto1-1.png",
     price: 100,
     company: "Mancile Café",
     info:
@@ -14,7 +14,7 @@ export const storeProducts = [
   {
     id: 2,
     title: "Amaranto",
-    img: "img/producto2.jpeg",
+    img: "img/producto2.png",
     price: 25,
     company: "Amaranto",
     info:
@@ -26,7 +26,7 @@ export const storeProducts = [
   {
     id: 3,
     title: "504 Cake Shop",
-    img: "img/producto3.jpg",
+    img: "img/producto3-2.png",
     price: 40,
     company: "504 Cake Shop",
     info:
@@ -38,7 +38,7 @@ export const storeProducts = [
   {
     id: 4,
     title: "Spooner's",
-    img: "img/producto4.jpg",
+    img: "img/producto4.png",
     price: 90,
     company: "Spooner's",
     info:
@@ -50,7 +50,7 @@ export const storeProducts = [
   {
     id: 5,
     title: "Miel Trigona",
-    img: "img/producto5.jpg",
+    img: "img/producto5.png",
     price: 120,
     company: "Miel Trigona",
     info:
@@ -62,9 +62,33 @@ export const storeProducts = [
   {
     id: 6,
     title: "Palomitas Gourmet",
-    img: "img/producto6.jpg",
+    img: "img/producto6.png",
     price: 50,
     company: "Palomitas Gourmet",
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "504 Cake Shop",
+    img: "img/producto7.png",
+    price: 35,
+    company: "504 Cake Shop",
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Amaranto",
+    img: "img/producto8.png",
+    price: 20,
+    company: "Amaranto",
     info:
       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
     inCart: false,
